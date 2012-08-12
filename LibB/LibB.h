d14eba522bc6e0ce6a00b64b1f658ee015b3233f
Daniel Tull <dt@danieltull.co.uk>
2012-08-12 18:14:58 +0100

    Initial Commit

---
+++ LibB/LibB.h
@@ -0,0 +1,13 @@
+//
+//  LibB.h
+//  LibB
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibB : NSObject
+
+@end
---
+++ LibB/LibB.m
@@ -0,0 +1,13 @@
+//
+//  LibB.m
+//  LibB
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "LibB.h"
+
+@implementation LibB
+
+@end
